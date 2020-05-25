9acd2343e39341f81b0ab315d0a884a8f6c13fe9
kubakakauko <kubakakauko@gmail.com>
2020-05-25 02:19:23 +0100

    Bunch of changes

--- ScriptsTest/Recoil.cpp
+++ ScriptsTest/Recoil.cpp
@@ -18,15 +18,14 @@ double fov = 90.0f;
 int selectedWeapon = -1; //Checks what weapon is selected | -1 none | 0 AK |
 bool scriptActive = false;
 #pragma endregion
-
-void mouse_move(Math::Vector2 vector2) //Mouse Movement (For now takes integers but want to be reprogrammed to take in Vector2)
+void mouse_move(int x, int y) //Mouse Movement (For now takes integers but want to be reprogrammed to take in Vector2)
 {
 	INPUT input;
 	input.type = INPUT_MOUSE;
 	input.mi.mouseData = 0;
 	input.mi.time = 0;
-	input.mi.dx = (int) vector2.x;
-	input.mi.dy = (int) vector2.y;
+	input.mi.dx = x;
+	input.mi.dy = y;
 	input.mi.dwFlags = MOUSEEVENTF_MOVE;
 	SendInput(1, &input, sizeof(input));
 }
@@ -34,10 +33,11 @@ int KeyHandler() // Determines if the script was activated.
 {
 	for(;;)
 	{
+		
 		Sleep(10);
+		//Activate the scripts
 		if (GetAsyncKeyState(VK_F2))
-		{
-			Sleep(100);
+		{   Sleep(100);
 			scriptActive = !scriptActive;
 			
 			if(scriptActive) 
@@ -48,7 +48,9 @@ int KeyHandler() // Determines if the script was activated.
 				Beep(100, 200);
 			}	
 		}
-		if (GetAsyncKeyState(VK_NUMPAD0) < 0) // AK-47
+		
+		// AK-47
+		if (GetAsyncKeyState(VK_NUMPAD0) < 0)
 		{
 			if(selectedWeapon != -1)
 			{
@@ -65,7 +67,6 @@ int KeyHandler() // Determines if the script was activated.
 		Sleep(200);
 	}	
 }
-
 #pragma region Recoil Delta Return
 
 float* weaponDeltaX(int selected)
@@ -127,33 +128,100 @@ int weaponAmmo(int selected)
 }
 #pragma endregion Recoil Delta Return
 
-void Recoil()
+void smoothRecoil(float x, float y, float animationTime)
 {
-	while (true)
+	//Creating a new timer counting elapsed time
+	Timer timer;
+	//Creating two different vectors one inheriting from the method, the other one returning to 0
+	Math::Vector2 targetMove = { x, y };
+	Math::Vector2 totalMoved = { 0.0f, 0.0f };
+
+
+	
+	while(timer.Elapsed() < animationTime)
 	{
+		float t = timer.Elapsed() / animationTime;
+
+		Math::Vector2 moveTo = (targetMove.Lerp(t) - totalMoved).Floor();
+
+		mouse_move(static_cast<int>(moveTo.x), static_cast<int>(moveTo.y));
+
+		totalMoved += moveTo;
+
+		SleepEx(1, false);
+	}
+	
+	Math::Vector2 extraMove = (targetMove - totalMoved).Floor();
+	mouse_move(static_cast<int>(extraMove.x), static_cast<int>(extraMove.y));
+}
+
+void calculations()
+{
+	//Forever
+	for(;;)
+	{
+		//While Left and Right mouse buttons  && weapon is selected && scripts are active 
 		while ((GetAsyncKeyState(1) & 0x8000) && (GetAsyncKeyState(2) & 0x8000 && selectedWeapon != -1 && scriptActive == true))
 		{
-			
+			//Do as many times as the amount of ammo
 			for (int i = 0; i < weaponAmmo(selectedWeapon); i++)
 			{
+				//If either of mouse buttons is not pressed stop the code.
 				if (!(GetAsyncKeyState(1) & 0x8000) || !(GetAsyncKeyState(2) & 0x8000))
 				{
-					continue;
+					break;
 				}
-
-				Math::Vector2 delta; delta.x = weaponDeltaX(selectedWeapon)[i + 1] - weaponDeltaX(selectedWeapon)[i]; delta.y = weaponDeltaY(selectedWeapon)[i + 1] - weaponDeltaY(selectedWeapon)[i];
+				
+				Math::Vector2 delta;
+				//if there is not more ammunition stop the code to prevent weird recoils
+				if(i == weaponAmmo(selectedWeapon))
+				{
+					break;
+				}
+				else
+				{
+					//Sets the value to change of delta between current and previous shot.
+					delta.x = weaponDeltaX(selectedWeapon)[i + 1] - weaponDeltaX(selectedWeapon)[i];
+					delta.y = weaponDeltaY(selectedWeapon)[i + 1] - weaponDeltaY(selectedWeapon)[i];
+				}
+				
+				
+				//Used to calculate the FOV and SENS multiplier 
 				float multiplier = -0.03f * (sens * 3.0f) * (fov / 100.0f);
-				Math::Vector2 px; px.x = delta.x / multiplier; px.y = delta.y / multiplier;
 
-				mouse_move(px);
-				Sleep(weaponTime(selectedWeapon));
+
+				//Actual Pixel values | No-Recoil works with these however no smoothing
+				float x = delta.x / multiplier;
+				float y = delta.y / multiplier;
+
+				//The time it takes for the gun to "Jump" (Unsure about this one)
+				float animationTime = (sqrt((delta.x * delta.x) + (delta.y * delta.y)) / 0.02f);
+
+
+
+				//Runs the function responsible for recoil movement
+				smoothRecoil(x, y, animationTime);
+				//if there is a negative weapon time, go to sleep
+				if (weaponTime(selectedWeapon) - animationTime > 0.0f) { SleepEx(static_cast<int>(weaponTime(selectedWeapon) - animationTime), false); }
 
 			}
-		}	
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
 void gameSettings()
 {
+	Clear();
 	string sSensitivity;
 	string sFov;
 	
@@ -167,9 +235,24 @@ void gameSettings()
 	fov = ::atof(sFov.c_str());
 	Clear();
 }
+void drawing()
+{
+	
+	
+}
+
 void main()
 {
+	//starts a thread taking detecting the keypresses 
+	CreateThread(0, 0, (LPTHREAD_START_ROUTINE)KeyHandler, 0, 0, 0);
 
+	//If the scripts are not active, don't proceed 
+	while(!scriptActive)
+	{
+		cout << "Press F2 To Turn the scripts on"<<endl;
+		Sleep(1000);
+	}
+	
 	gameSettings();
 	cout << "Welcome to COMBO-X\n";
 	cout << "------------------\n";
@@ -178,13 +261,14 @@ void main()
 	cout << "SAR      = 3\n";
 	cout << "THOMPSON = 4\n\n\n";
 
+	//starts threads
+	CreateThread(0, 0, (LPTHREAD_START_ROUTINE)calculations, 0, 0, 0);
 
-	CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Recoil, 0, 0, 0);
-	CreateThread(0, 0, (LPTHREAD_START_ROUTINE)KeyHandler, 0, 0, 0);
 
+	//Puts the theads to sleep to decrease CPU load
 	for (;;)
 	{
-		Sleep(200);
+		Sleep(50);
 	}
 
 }
--- ScriptsTest/Weapons.h
+++ ScriptsTest/Weapons.h
@@ -4,12 +4,12 @@
 namespace AssaultRifle{
 
 	int AssaultRifleAmmo= 30;
-	float AssaultRifleX[] = {0.0f, 1.390706f , 1.176434f, 3.387171f, 5.087049f, 5.094189f, 4.426013f, 3.250455f, 1.73545f, 0.04893398f, -1.641158f, -3.166891f, -4.360331f, -5.053545f, -5.090651f, -4.489915f,-3.382552f, -1.899585f, -0.1720295f, 1.669086f, 3.492748f, 5.16793f, 6.563614f, 7.548776f, 7.992399f, 7.512226f, 6.063792f, 4.117367f, 2.143932f, 0.6144824f, 0.0f};
-	float AssaultRifleY[] = {0.0f, -2.003941f , -3.844176f, -5.516686f, -7.017456f, -8.342467f, -9.487704f, -10.44915f, -11.22279f, -11.8046f, -12.19056f, -12.58713f, -13.32077f, -14.32128f, -15.51103f, -16.81242f, -18.14783f, -19.43966f, -20.61031f, -21.58213f, -22.27755f, -22.61893f, -22.81778f, -23.37389f, -24.21139f, -25.23734f, -26.35886f, -27.48302f, -28.51692f, -29.36766f, 0.0f};
+	float AssaultRifleX[] = {0.0f, 1.390706f, 1.176434f, 3.387171f, 5.087049f, 5.094189f, 4.426013f, 3.250455f, 1.73545f, 0.04893398f, -1.641158f, -3.166891f, -4.360331f, -5.053545f, -5.090651f, -4.489915f, -3.382552f, -1.899585f, -0.1720295f, 1.669086f, 3.492748f, 5.16793f, 6.563614f, 7.548776f, 7.992399f, 7.512226f, 6.063792f, 4.117367f, 2.143932f, 0.6144824f};
+	float AssaultRifleY[] = {0.0f, -2.003941f, -3.844176f, -5.516686f, -7.017456f, -8.342467f, -9.487704f, -10.44915f, -11.22279f, -11.8046f, -12.19056f, -12.58713f, -13.32077f, -14.32128f, -15.51103f, -16.81242f, -18.14783f, -19.43966f, -20.61031f, -21.58213f, -22.27755f, -22.61893f, -22.81778f, -23.37389f, -24.21139f, -25.23734f, -26.35886f, -27.48302f, -28.51692f, -29.36766f};
     float AssaultRifleTimes = 1000.0f / (450.0f / 60.0f);
 }
 
-
+//This namespace can be used when there is nothing selected
 namespace defaultNothingNamespace
 {
 	float defaultNothing[] = { 0, 0 };
@@ -18,6 +18,7 @@ namespace defaultNothingNamespace
 
 namespace classes
 {
+	//Class which lets clearing the console
     void Clear() {
         COORD topLeft = { 0, 0 };
         HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -34,6 +35,7 @@ namespace classes
         );
         SetConsoleCursorPosition(console, topLeft);
     }
+
     
 }
 class Timer
@@ -46,6 +48,7 @@ public:
     {
         // Store the time at which this timer was instantiated.
         QueryPerformanceCounter(&starting_time);
+    	
     }
 
     /**
